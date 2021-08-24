20ca53d7b1bca612fff481302314ed014f0856af
Thomas <thomas@craftacademy.se>
2021-08-24 07:25:55 +0200

    WIP small experiments with adding methods at runtime based on attributes and method calls

--- app/models/app_data.rb
+++ app/models/app_data.rb
@@ -6,4 +6,25 @@ class AppData
   def self.as_json
     data.to_json
   end
+
+  def self.sections
+    data[:app_data].keys
+  end
+
+  def self.method_missing(*args, &block)
+    super unless sections.include?(*args)
+    item = data[:app_data][*args]
+    case item
+    when Array
+      item
+    when Hash
+      if block_given? && block.yield.instance_of?(Symbol)
+        item[block.yield]
+      else
+        item
+      end
+    else
+      item
+    end
+  end
 end
