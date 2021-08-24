d8630eae46de3a02907b2f2bfc5d54a7eb2cbd92
Thomas <thomas@craftacademy.se>
2021-08-24 09:14:30 +0200

    WIP update method

--- app/models/app_data.rb
+++ app/models/app_data.rb
@@ -27,4 +27,21 @@ class AppData
       item
     end
   end
+
+  # Usage
+  # AppData.update(<section>, <content>)
+  def self.update(*args)
+    section, update = *args
+    raise 'no such section' unless sections.include?(section)
+    content = send(section)
+    case content
+    when Array
+      new_content = { section => [*content, *update] }
+    when Hash
+      new_content = { section => { **content, **update } }
+    end
+    new_data = { **data[:app_data], **new_content }
+    yaml = { app_data: new_data }.to_yaml
+    File.open(Rails.root.join('lib', 'app_data.yml'), 'w') { |f| f.write yaml }
+  end
 end
