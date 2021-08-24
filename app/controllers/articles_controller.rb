efd51e7d16fdff6f4ff9e248dc4d6e9bda6441e1
Thomas <thomas@craftacademy.se>
2021-08-24 15:59:47 +0200

    adds serializers for article with some hard-coded data

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
   def index
     articles = Article.all
-    render json: { articles: articles }
+    render json: articles, each_serializer: Article::IndexSerializer
   end
 
   def show
     article = Article.find(params[:id])
-    render json: { article: article }
+    render json: article, serializer: Article::ShowSerializer
   end
 end
---
+++ app/serializers/article/show_serializer.rb
@@ -0,0 +1,14 @@
+class Article::ShowSerializer < ActiveModel::Serializer
+  attributes :id, :id, :title, :body, :date, :image
+
+  def date
+    object.created_at.strftime('%Y-%m-%d')
+  end
+
+  def image
+    {
+      url: 'https://healthtechmagazine.net/sites/healthtechmagazine.net/files/styles/cdw_hero/public/articles/%5Bcdw_tech_site%3Afield_site_shortname%5D/202007/20200630_HT_Web_MonITor_Tech-Organizations-Should-Consider.jpg?itok=adOWwJ9x',
+      alt: 'Nice doctor picture'
+    }
+  end
+end
