faa7747ca5fc6f226e1b49ee0598c1d7394c6214
sevader14 <vsevolod.deriushkin@gmail.com>
2021-08-30 11:34:07 +0200

    fixes header API_KEY check, updates app_data.yml to most recent version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::API
   private
 
   def authenticate_request
-    api_key = request.env['API_KEY']
+    api_key = request.env['API_KEY'] || request.env['HTTP_API_KEY']
     unless Rails.application.credentials.client_api_keys.include? api_key
       render json: { message: 'wrong api key' },
              status: :unauthorized
