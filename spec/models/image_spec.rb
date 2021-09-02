86998748217e6ab350e9ca0b93383d128bad762c
Thomas <thomas@craftacademy.se>
2021-09-02 09:36:41 +0200

    reinstates it block

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe Image, type: :model do
       expect(create(:image, article: article)).to be_valid
     end
 
-    # it 'is expected to have valid basic Factory' do
-    #   expect(build(:associated_image)).to be_valid
-    # end
+    it 'is expected to have valid basic Factory' do
+      expect(build(:associated_image)).to be_valid
+    end
   end
   describe 'db table' do
     it { is_expected.to have_db_column(:alt_text).of_type(:string) }
