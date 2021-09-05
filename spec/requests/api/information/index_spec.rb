65cc8b587373c5b6ec93a5ca743c4f42acc26b6f
sevader14 <vsevolod.deriushkin@gmail.com>
2021-09-05 16:38:25 +0200

    adds index spec

---
+++ spec/requests/api/information/index_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe 'GET /api/information', type: :request do
+  let!(:api_key) { Rails.application.credentials.client_api_keys[0] }
+
+  subject { response }
+
+  describe 'with valid api key' do
+    before do
+      get '/api/information', headers: { API_KEY: api_key }
+    end
+
+    it { is_expected.to have_http_status 200 }
+
+    it 'is expected to respond with a list of 3 information items' do
+      expect(response_json['information_items'].count).to eq 3
+    end
+  end
+
+  describe 'with invalid api key' do
+    before do
+      get '/api/information', headers: { API_KEY: 'invalid_key' }
+    end
+
+    it { is_expected.to have_http_status 401 }
+    
+  end
+end
