ade98ebc78607df6547dd6cf3d15d6000147623a
sevader14 <vsevolod.deriushkin@gmail.com>
2021-09-10 20:03:59 +0200

    adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,22 @@ about_self_care_view = View.create(name: 'about_self_care', variant: 1)
 information_view = View.create(name: 'information', variant: 1)
 services_view = View.create(name: 'services', variant: 0)
 
+puts 'creating image...'
+# image = Image.create(
+#   url: 'https://assets.zyto.com/wp-content/uploads/2018/05/holistic-health-concept-woman-doing-yoga-on-beach-at-dusk.jpg',
+#   alt_text: 'Alt attribute'
+# )
+image_file = URI.open('https://assets.zyto.com/wp-content/uploads/2018/05/holistic-health-concept-woman-doing-yoga-on-beach-at-dusk.jpg')
+image = Image.create(alt_text: 'Alt attribute')
+image.file.attach(io: image_file, filename: 'article_image.jpg', content_type: 'image/jpg')
+
 puts 'creating sections...'
 section_regular = Section.create(
   view_id: information_view.id,
   header: 'Section regular',
   description: 'A Solid App is composed of functions that we call Components. Component names in Solid follow the Pascal naming convention in which the first letter of each word in a component name is capitalized. This name can then be used as tags within our JSX like',
   variant: 0,
-  image:  
-  alt: 'Alt attribute'
+  image: image
 )
 section_no_image = Section.create(
   view_id: about_self_care_view.id,
