b9d0a73c9e58dc9e08e93191240025bcf9117384
luke <lukeperrin27@gmail.com>
2021-09-14 16:42:35 +0200

    adds mail method for resetting password email includes link redirect to page for new password

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,30 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails"
+require 'rails'
 require 'active_storage/engine'
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require_relative '../lib/convert.rb'
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require_relative '../lib/convert'
 Bundler.require(*Rails.groups)
 
 module KCSCApi
   class Application < Rails::Application
+    config.exceptions_app = routes
+    config.action_mailer.delivery_method = 'smtp'
+    config.action_mailer.smtp_settings = {
+      port: 587,
+      user_name: 'apikey',
+      password: Rails.application.credentials.sendgrid_api_key,
+      address: 'smtp.sendgrid.net',
+      domain: 'heroku.com',
+      authentication: 'plain'
+    }
+    config.action_mailer.default_url_options = { host: 'https://kcsc-api.herokuapp.com' }
+
     config.load_defaults 6.1
     config.api_only = true
     config.generators do |generate|
