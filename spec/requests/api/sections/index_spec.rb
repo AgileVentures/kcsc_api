d60e23a9db90516bacbb9b4d54d32d549713e34b
sevader14 <vsevolod.deriushkin@gmail.com>
2021-10-26 10:03:21 +0200

    adds tests

--- spec/requests/api/sections/index_spec.rb
+++ spec/requests/api/sections/index_spec.rb
@@ -1,10 +1,14 @@
 RSpec.describe 'GET /sections?view=:view_name', type: :request do
   let!(:api_key) { Rails.application.credentials.client_api_keys[0] }
   let!(:about_view) { create(:view, name: 'about') }
-  let!(:sections) { create_list(:section, 3, view: about_view, variant: :regular) }
+  let!(:sections) do
+    create_list(:section, 2, view: about_view, variant: :regular) do |section, i|
+      section.order = 2 - i
+    end
+  end
+
   subject { response }
 
-  
   describe 'with valid api key' do
     before do
       get "/api/sections?view=#{about_view.name}", headers: { API_KEY: api_key }
@@ -13,7 +17,12 @@ RSpec.describe 'GET /sections?view=:view_name', type: :request do
     it { is_expected.to have_http_status 200 }
 
     it 'is expected to return a list of sections' do
-      expect(response_json['sections'].length).to eq(3)
+      expect(response_json['sections'].length).to eq(2)
+    end
+
+    it 'is expected to return a list of sections sorted by order' do
+      expected_outcome = [{ order: 1 }, { order: 2 }]
+      expect(response_json['sections']).to eq(expected_outcome)
     end
   end
 end
